b8ae95d0bc2bed2a4204c3506d2baf4bdc40e9ca
William Bert <william.bert@gmail.com>
2013-01-21 15:13:55 -0500

    Confirm package install.

--- setup-package.el
+++ setup-package.el
@@ -1,18 +1,17 @@
 (require 'package)
 
-(defvar marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
 (defvar gnu '("gnu" . "http://elpa.gnu.org/packages/"))
 (defvar melpa '("melpa" . "http://melpa.milkbox.net/packages/"))
+(defvar marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
 
-;; Add marmalade to package repos
+(add-to-list 'package-archives melpa)
 (add-to-list 'package-archives marmalade)
-(add-to-list 'package-archives melpa t)
 
 (package-initialize)
 
 (unless (and (file-exists-p "~/.emacs.d/elpa/archives/marmalade")
-             (file-exists-p "~/.emacs.d/elpa/archives/gnu")
              (file-exists-p "~/.emacs.d/elpa/archives/melpa"))
+             (file-exists-p "~/.emacs.d/elpa/archives/gnu")
   (package-refresh-contents))
 
 (defun packages-install (&rest packages)
@@ -20,9 +19,10 @@
           (let ((name (car package))
                 (repo (cdr package)))
             (when (not (package-installed-p name))
-              (let ((package-archives (list repo)))
-                (package-initialize)
-                (package-install name)))))
+              (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+                  (let ((package-archives (list repo)))
+                    (package-initialize)
+                    (package-install name))))))
         packages)
   (package-initialize)
   (delete-other-windows))
