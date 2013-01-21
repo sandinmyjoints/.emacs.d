b0f1cf5bf1ebf3f173f8c1776d6b45cfacb1ae04
William Bert <william.bert@gmail.com>
2013-01-21 16:32:13 -0500

    Add defuns.el.

---
+++ defuns.el
@@ -0,0 +1,100 @@
+(defun find-in-project (path grep-string)
+  "rgrep in current project dir."
+  (interactive (list (read-directory-name "path: " find-in-project-default-dir)
+                     (read-from-minibuffer "find: ")))
+  (let ((default-directory path))
+    (grep-find
+     (concat "find . " find-args grep-string))))
+
+(defun toggle-window-dedicated ()
+  "Toggle whether the current active window is dedicated or not"
+  (interactive)
+  (message
+   (if (let (window (get-buffer-window (current-buffer)))
+         (set-window-dedicated-p window
+                                 (not (window-dedicated-p window))))
+       "'%s' is dedicated"
+     "'%s' is normal")
+   (current-buffer)))
+
+(defun whack-whitespace (arg)
+  "Delete all white space from point to the next word.  With prefix ARG
+    delete across newlines as well.  The only danger in this is that you
+    don't have to actually be at the end of a word to make it work.  It
+    skips over to the next whitespace and then whacks it all to the next
+    word."
+  (interactive "P")
+  (let ((regexp (if arg "[ \t\n]+" "[ \t]+")))
+    (re-search-forward regexp nil t)
+    (replace-match "" nil nil)))
+
+(defun fix-html ()
+  "Fix HTML."
+  (interactive)
+  (sgml-pretty-print (point-min) (point-max))
+  (indent-region (point-min) (point-max)))
+
+(defun wjb-get-marker-replacer (marker)
+  "Returns a marker-replacer function for `marker`."
+  (lambda ()
+    (interactive)
+    (save-excursion
+      (while (search-forward marker nil t)
+        (replace-match "" nil t)))))
+
+;; Shortcut to clear marker from test config file.  TODO: When
+;; turning on, if grep is unsuccessful, insert new grep under all
+;; greps (most specific).
+;;;
+(defun wjb-toggle-marker-in-buffer (arg marker)
+  "Toggle `marker` on or off in `wjb-test-config-buffer`."
+  (interactive "P\nsMarker: ")
+  (with-current-buffer wjb-test-config-buffer
+    (let ((commented (concat " grep: \"" marker "\""))
+          (uncommented (concat " #grep: \"" marker "\"")))
+      (progn
+        (beginning-of-buffer)
+        (cond ((null arg)
+               ;; Null arg. Turn on.
+               (unless (null (re-search-forward uncommented (point-max) t))
+                 (replace-match commented)))
+              (t
+               ;; Non-null arg. Turn off.
+               (unless (null (re-search-forward commented (point-max) t))
+                 (replace-match uncommented))))
+        (back-to-indentation)
+        (save-buffer)))))
+
+(defun wjb-toggle-marker (arg marker &optional handle-in-current-buffer)
+  (interactive "P\nsMarker: ")
+  (cond
+   ((null arg)  ;; Turn on marker.
+    (progn
+      ;; Insert marker in this buffer.
+      (unless (null handle-in-current-buffer)
+        (insert marker)
+        (save-buffer))
+      ;; Turn on marker in test config.
+      (wjb-toggle-marker-in-buffer nil marker)
+      (message (concat marker " tests on."))))
+   (t           ;; Turn off marker.
+    (progn
+      (unless (null handle-in-current-buffer)
+        ;; Get a marker-replacer and use it in this buffer.
+        (command-execute
+         (wjb-get-marker-replacer marker))
+        (save-buffer))
+      ;; Turn off the marker in test config.
+      (wjb-toggle-marker-in-buffer t marker)
+      (message (concat marker " tests off."))))))
+
+;; Journal command.
+(defun journal ()
+	"Start journaling"
+	(interactive)
+	(switch-to-buffer "journal")
+	(text-mode)
+	(auto-fill-mode 1)
+	(set-fill-column 80))
+
+(provide 'defuns)
