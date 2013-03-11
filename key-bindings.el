b96162a941dfc137861ecfee4581a247020d0c3a
William Bert <william.bert@gmail.com>
2013-03-10 20:20:20 -0400

    Fix when for smex.

--- key-bindings.el
+++ key-bindings.el
@@ -52,10 +52,10 @@
 
 
 ;; Smex.
-(when ((fboundp 'smex)
-       (global-set-key (kbd "M-x") 'smex)
-       (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-       ;; This is your old M-x.
-       (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
+(when (fboundp 'smex)
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  ;; This is your old M-x.
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
 
 (provide 'key-bindings)
