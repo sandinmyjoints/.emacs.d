edbdad7cebf8aafb6d81400bb6c70e544fe6041e
William Bert <william@spanishdict.com>
2013-10-11 12:05:11 -0400

    Use json-mode for *.json.

--- mode-mappings.el
+++ mode-mappings.el
@@ -30,7 +30,7 @@
 ;; JavaScript
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
 ;(add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
 ;; Add buffer-local indicator for whether prog-mode-hook has run.
