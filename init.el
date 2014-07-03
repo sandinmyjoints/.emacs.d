b6c111e742bb6125eb1b161ebbe2966f6928ffc7
William Bert <william@spanishdict.com>
2014-07-03 10:49:08 -0400

    Load magit-blame.

--- init.el
+++ init.el
@@ -336,6 +336,7 @@ and overlay is highlighted between MK and END-MK."
 (autoload 'magit-log "magit")
 (eval-after-load 'magit '(require 'setup-magit))
 (eval-after-load 'magit '(require 'rebase-mode))
+(eval-after-load 'magit '(require 'magit-blame))
 
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 
