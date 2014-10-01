6ccc6999304bf870e8ad826553a7fe6eb5661451
William Bert <william@spanishdict.com>
2014-10-01 09:25:52 -0700

    Add headers.

--- setup-ido.el
+++ setup-ido.el
@@ -1,3 +1,51 @@
+;;; setup-ido.el --- Configure ido-mode.
+;;
+;; Filename: setup-ido.el
+;; Description:
+;; Author: William
+;; Maintainer:
+;; Created: Wed Oct  1 09:26:00 2014 (-0700)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 0
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
 ;; Ido.
 (when (require 'ido nil t)
   (ido-mode t)
@@ -20,11 +68,14 @@
   ;; TODO: extensions order, ignore
   ;; (setq ido-file-extensions-order '(".org" ".txt" ".py" ".emacs" ".xml" ".el" ".ini" ".cfg" ".cnf"))
 
-  ;; Configure flx to work with ido.
-  (flx-ido-mode 1)
-  ;; disable ido faces to see flx highlights.
   (setq ido-enable-flex-matching t)
-  (setq ido-use-faces nil)
+
+  ;; Configure flx to work with ido.
+  ; (flx-ido-mode 1) ;; problem is it doesn't give value to recently visited buffers.
+
+  ;; disable ido faces to see flx highlights.
+  (setq ido-use-faces t)
+
   (setq flx-ido-threshold 8192)
 
   ;; per https://github.com/lewang/flx
@@ -33,3 +84,6 @@
   )
 
 (provide 'setup-ido)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; setup-ido.el ends here
--- setup-package.el
+++ setup-package.el
@@ -1,3 +1,51 @@
+;;; setup-package.el --- Configure package management and install packages.
+;;
+;; Filename: setup-package.el
+;; Description:
+;; Author: William
+;; Maintainer:
+;; Created: Wed Oct  1 09:24:43 2014 (-0700)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 0
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
 ;; Based on:
 ;; https://github.com/magnars/.emacs.d/blob/master/setup-package.el
 
@@ -99,3 +147,6 @@ re-downloaded in order to locate PACKAGE."
         (require-package package min-version t)))))
 
 (provide 'setup-package)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; setup-package.el ends here
