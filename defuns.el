93827007b42bc1db1f30c581fb34b241cf04e852
William Bert <william@spanishdict.com>
2014-10-08 16:55:24 -0700

    Add create-tags.

--- defuns.el
+++ defuns.el
@@ -464,4 +464,20 @@ following line."
    (interactive "*p")
    (move-text-internal (- arg)))
 
+;; etags / ctags
+(cond ((eq system-type 'gnu/linux)
+       (setq path-to-ctags "/usr/local/bin/ctags")))
+(cond ((eq system-type 'darwin)
+       (setq path-to-ctags "/usr/local/bin/ctags")))
+
+(defun create-tags (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  ;; (message
+  ;;  (format "%s -f %s/tags -eR %s" path-to-ctags (directory-file-name dir-name) (directory-file-name dir-name)))
+  (shell-command
+   (format "%s -f %s/tags -eR %s" path-to-ctags
+           (directory-file-name dir-name) (directory-file-name dir-name))))
+
+
 (provide 'defuns)
