fc20ae0af1a9e61a6120dc81ea1ab6b4dd0cfeca
William Bert <william@spanishdict.com>
2015-07-22 17:58:52 -0700

    Upgrade magit.

--- setup-lisp/setup-magit.el
+++ setup-lisp/setup-magit.el
@@ -1,24 +1,33 @@
 ;; Magit.
 
 ;;; Code:
-(when (require 'magit nil t)
-  ;; change magit diff colors
-  (eval-after-load 'magit
-    '(progn
-       ;; Use list-faces-display to examine all faces.
-       (set-face-foreground 'magit-diff-add "green3") ;; Normal by default.
-       (set-face-foreground 'magit-diff-del "red3") ;; Normal by default.
-       (set-face-background 'magit-diff-add "black") ;; Dull green by default.
-       (set-face-background 'magit-diff-del "black") ;; Dull red by default.
-       (set-face-background 'magit-item-highlight "#101212")
-       (set-face-background 'magit-diff-none "black") ;; Black by default.
-       (unless (display-graphic-p)
-         (set-face-background 'magit-item-highlight "black"))
 
-       (set-face-attribute 'magit-item-highlight nil
-                           :weight 'normal)))
+;;(when (require 'magit nil t))
 
-  (autoload 'magit-blame "magit-blame-mode" "Minor mode for blaming." t))
+;; Old magit diff colors.
+;; '(progn
+;;    ;; Use list-faces-display to examine all faces.
+;;    (set-face-foreground 'magit-diff-add "green3") ;; Normal by default.
+;;    (set-face-foreground 'magit-diff-del "red3") ;; Normal by default.
+;;    (set-face-background 'magit-diff-add "black") ;; Dull green by default.
+;;    (set-face-background 'magit-diff-del "black") ;; Dull red by default.
+;;    (set-face-background 'magit-item-highlight "#101212")
+;;    (set-face-background 'magit-diff-none "black") ;; Black by default.
+;;    (unless (display-graphic-p)
+;;      (set-face-background 'magit-item-highlight "black"))
+
+;;    (set-face-attribute 'magit-item-highlight nil
+;;                        :weight 'normal))
+
+(set-face-bold 'magit-diff-file-heading nil)
+(set-face-background 'magit-diff-context-highlight "grey10")
+(set-face-background 'magit-diff-context "grey10")
+
+(setq magit-last-seen-setup-instructions "1.4.0"
+      magit-revert-buffers 'silent
+      magit-diff-auto-show '(stage-all log-oneline log-follow log-select blame-follow))
+
+(define-key magit-status-mode-map (kbd "M-u") 'magit-section-up)
 
 (setq magit-completing-read-function 'magit-ido-completing-read)
 
