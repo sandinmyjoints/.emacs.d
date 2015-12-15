53d9055a653ae4db1ccbb98764e3e0abe2a8f255
William Bert <william@spanishdict.com>
2015-12-15 14:08:11 -0800

    Update magit setup.

--- setup-lisp/setup-magit.el
+++ setup-lisp/setup-magit.el
@@ -24,14 +24,15 @@
 (set-face-background 'magit-diff-context "grey10")
 
 (setq magit-last-seen-setup-instructions "1.4.0"
-      magit-revert-buffers 'silent
-      magit-diff-auto-show '(stage-all log-oneline log-follow log-select blame-follow))
-
-(define-key magit-status-mode-map (kbd "M-u") 'magit-section-up)
-
-(setq magit-completing-read-function 'magit-ido-completing-read
+      magit-diff-auto-show '(stage-all log-oneline log-follow log-select blame-follow)
+      magit-status-expand-stashes nil
+      magit-commit-show-diff nil
+      magit-revert-buffers 1 ;; important for not slowing down everything
+      magit-completing-read-function 'magit-ido-completing-read
       magit-push-always-verify nil
       magit-branch-read-upstream-first nil)
+
+(define-key magit-status-mode-map (kbd "M-u") 'magit-section-up)
 
 ;; magit-gh-pulls
 ;; This was useful: https://github.com/sigma/magit-gh-pulls/issues/5
