3bbdf0f50f85798ff64b72e6f5295697624feba8
William Bert <william@spanishdict.com>
2016-06-29 16:40:00 -0700

    Make header for js2-mode.

--- setup-lisp/setup-js2-mode.el
+++ setup-lisp/setup-js2-mode.el
@@ -1,3 +1,51 @@
+;;; setup-js2-mode.el --- Set up js2-mode.
+;;
+;; Filename: setup-js2-mode.el
+;; Description:
+;; Author: William
+;; Maintainer:
+;; Created: Wed Jun 29 16:39:41 2016 (-0700)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 0
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
 (require-package 'json-mode)
 
 (when (>= emacs-major-version 24)
@@ -255,4 +303,6 @@
 (set-face-foreground 'js2-object-property "light goldenrod")
 
 (provide 'setup-js2-mode)
-;;; setup-js2-mode ends here
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; setup-js2-mode.el ends here
