66317b1def82494f49cda97121be1bad208894e0
William Bert <william@spanishdict.com>
2016-10-23 22:27:49 -0700

    Specific mode mapping.

--- setup-lisp/mode-mappings.el
+++ setup-lisp/mode-mappings.el
@@ -19,7 +19,10 @@
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+
 (add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\neodarwin-site.erb$" . nginx-mode))
+
 (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
 
 ;; Clojure
