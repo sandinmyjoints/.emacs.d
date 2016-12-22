7203f583a57a27b7de323f642c5267d05ad4431d
William Bert <william@spanishdict.com>
2016-12-22 09:47:15 -0800

    Magit.

--- setup-lisp/setup-magit.el
+++ setup-lisp/setup-magit.el
@@ -40,4 +40,22 @@
 ;;(require 'magit-gh-pulls)
 ;;(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
 
+
+;; From http://endlessparentheses.com/create-github-prs-from-emacs-with-magit.html
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/pull/new/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-push-remote)
+                       "url"))
+           (magit-get-current-branch))))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "v"
+     #'endless/visit-pull-request-url))
+
 (provide 'setup-magit)
