ca279311445b9a59f4b651395f4e5a2f86d74e8d
William Bert <william.bert@gmail.com>
2017-10-16 10:30:31 -0700

    Better way of opening grep in same window.

--- init.el
+++ init.el
@@ -260,90 +260,39 @@
                                           (interactive "P")
                                           (wjb-toggle-marker arg marker handle-in-current-buffer))))))
 
-;; Open grep results in the same frame. See:
-;; http://stackoverflow.com/questions/2299133/emacs-grep-find-link-in-same-window/2299261#2299261
-(eval-after-load "compile"
-  '(defun compilation-goto-locus (msg mk end-mk)
-     "Jump to an error corresponding to MSG at MK.
-All arguments are markers.  If END-MK is non-nil, mark is set there
-and overlay is highlighted between MK and END-MK."
-     ;; Show compilation buffer in other window, scrolled to this error.
-     (let* ((from-compilation-buffer (eq (window-buffer (selected-window))
-                                         (marker-buffer msg)))
-            ;; Use an existing window if it is in a visible frame.
-            (pre-existing (get-buffer-window (marker-buffer msg) 0))
-            (w (if (and from-compilation-buffer pre-existing)
-                   ;; Calling display-buffer here may end up (partly) hiding
-                   ;; the error location if the two buffers are in two
-                   ;; different frames.  So don't do it if it's not necessary.
-                   pre-existing
-                 (let ((display-buffer-reuse-frames t)
-                       (pop-up-windows t))
-                   ;; Pop up a window.
-                   (display-buffer (marker-buffer msg)))))
-            (highlight-regexp (with-current-buffer (marker-buffer msg)
-                                ;; also do this while we change buffer
-                                (compilation-set-window w msg)
-                                compilation-highlight-regexp)))
-       ;; Ideally, the window-size should be passed to `display-buffer' (via
-       ;; something like special-display-buffer) so it's only used when
-       ;; creating a new window.
-       (unless pre-existing (compilation-set-window-height w))
+;; Open grep results in the same frame.
+;;
+;; same-window-buffer-names
+;; * https://stackoverflow.com/questions/12231783/in-emacs-when-i-do-a-grep-find-how-can-i-have-the-files-displayed-to-me-open-i?noredirect=1&lq=1
+;; * https://stackoverflow.com/questions/15814031/want-compile-goto-error-variant-that-replaces-compilation-buffer-in-current-wind?noredirect=1&lq=1
+;;
+;; compile-goto-error is the function when I hit Enter on a grep result. It
+;; opens in a new frame. Why? Normal behavior may be to open compile results in
+;; a new window, but I have set pop-up-windows nil in sane-defaults so for me,
+;; compile results open in the same window. But then hitting enter opens in a
+;; new frame which still seems odd.
+;;
+;; I also want the same behavior from xref. It defines its own xref-goto-xref. L521
+;;
+;; This is from https://stackoverflow.com/a/20548556.
+;; It seems to work and is much smaller than the previous solution.
+(defadvice compile-goto-error (around my-compile-goto-error activate)
+  (let ((display-buffer-overriding-action '(display-buffer-reuse-window (inhibit-same-window . nil))))
+    ad-do-it))
 
-       (switch-to-buffer (marker-buffer mk))
+;; from https://emacs.stackexchange.com/a/34724/2163:
+(defvar my-inhibit-set-window-dedicated nil)
+(advice-add 'set-window-dedicated-p :around
+  (lambda (orig-fun &rest args)
+    "Honor inhibitor variable `my-inhibit-set-window-dedicated'."
+    (unless my-inhibit-set-window-dedicated
+      (apply orig-fun args))))
 
-       ;; If narrowing gets in the way of going to the right place, widen.
-       (unless (eq (goto-char mk) (point))
-         (widen)
-         (goto-char mk))
-       (if end-mk
-           (push-mark end-mk t)
-         (if mark-active (setq mark-active)))
-       ;; If hideshow got in the way of
-       ;; seeing the right place, open permanently.
-       (dolist (ov (overlays-at (point)))
-         (when (eq 'hs (overlay-get ov 'invisible))
-           (delete-overlay ov)
-           (goto-char mk)))
-
-       (when highlight-regexp
-         (if (timerp next-error-highlight-timer)
-             (cancel-timer next-error-highlight-timer))
-         (unless compilation-highlight-overlay
-           (setq compilation-highlight-overlay
-                 (make-overlay (point-min) (point-min)))
-           (overlay-put compilation-highlight-overlay 'face 'next-error))
-         (with-current-buffer (marker-buffer mk)
-           (save-excursion
-             (if end-mk (goto-char end-mk) (end-of-line))
-             (let ((end (point)))
-               (if mk (goto-char mk) (beginning-of-line))
-               (if (and (stringp highlight-regexp)
-                        (re-search-forward highlight-regexp end t))
-                   (progn
-                     (goto-char (match-beginning 0))
-                     (move-overlay compilation-highlight-overlay
-                                   (match-beginning 0) (match-end 0)
-                                   (current-buffer)))
-                 (move-overlay compilation-highlight-overlay
-                               (point) end (current-buffer)))
-               (if (or (eq next-error-highlight t)
-                       (numberp next-error-highlight))
-                   ;; We want highlighting: delete overlay on next input.
-                   (add-hook 'pre-command-hook
-                             'compilation-goto-locus-delete-o)
-                 ;; We don't want highlighting: delete overlay now.
-                 (delete-overlay compilation-highlight-overlay))
-               ;; We want highlighting for a limited time:
-               ;; set up a timer to delete it.
-               (when (numberp next-error-highlight)
-                 (setq next-error-highlight-timer
-                       (run-at-time next-error-highlight nil
-                                    'compilation-goto-locus-delete-o)))))))
-       (when (and (eq next-error-highlight 'fringe-arrow))
-         ;; We want a fringe arrow (instead of highlighting).
-         (setq next-error-overlay-arrow-position
-               (copy-marker (line-beginning-position)))))))
+(advice-add 'xref--show-pos-in-buf :around
+  (lambda (orig-fun &rest args)
+    "Inhibit `set-window-dedicated-p'."
+    (let ((my-inhibit-set-window-dedicated t))
+      (apply orig-fun args))))
 
 ;; ========================================
 ;; Some hooks.
