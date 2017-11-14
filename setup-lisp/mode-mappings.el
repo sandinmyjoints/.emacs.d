0ce3720d94429b3e2f11a2f4c21a9e232434b987
William Bert <william.bert@gmail.com>
2017-11-13 22:52:12 -0500

    mode-mapping alist pattern

--- setup-lisp/mode-mappings.el
+++ setup-lisp/mode-mappings.el
@@ -1,42 +1,43 @@
 ;; Jade and Stylus (sws = significant whitespace)
 (autoload 'sws-mode "sws-mode")
 (autoload 'jade-mode "jade-mode")
-(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
-(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+(add-to-list 'auto-mode-alist '("\\.styl\\'" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade\\'" . jade-mode))
 
 ;; HTML
-(add-to-list 'auto-mode-alist '("\\.htm$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.hbs$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.htm\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(setq-default web-mode-markup-indent-offset 2)
 
 ;; Ruby
 (autoload 'rhtml-mode "rhtml-mode")
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.watchr$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.watchr\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 
 (add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
 
-(add-to-list 'auto-mode-alist '("\\neodarwin-site.erb$" . nginx-mode))
+(add-to-list 'auto-mode-alist '("\\neodarwin-site.erb\\'" . nginx-mode))
 
 ;; Clojure
 (autoload 'clojure-mode "clojure-mode")
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-mode))
 
 ;; SVG
-(add-to-list 'auto-mode-alist '("\\.svg$" . image-mode))
+(add-to-list 'auto-mode-alist '("\\.svg\\'" . image-mode))
 
 ;; JavaScript
 (autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
 ;(add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
 ;; Add buffer-local indicator for whether prog-mode-hook has run.
@@ -56,11 +57,11 @@
 ;; Handlebars mode.
 (autoload 'handlebars-mode "handlebars-mode"
   "Major mode for editing Handlebars")
-(add-to-list 'auto-mode-alist '("\\.handlebars$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.handlebars\\'" . js2-mode))
 
 ;; Snippets
 (add-to-list 'auto-mode-alist '("yasnippet/snippets" . snippet-mode))
-(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
+(add-to-list 'auto-mode-alist '("\\.yasnippet\\'" . snippet-mode))
 
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode"
@@ -68,10 +69,6 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-(setq markdown-open-command "mark")
-
-;; org-mode
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
 ;; log mode
 (autoload 'log4j-mode "log4j-mode" "Major mode for viewing log files." t)
@@ -79,16 +76,14 @@
 
 ;; php
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
 
 ;; less-css-mode
 (autoload 'less-css-mode "less-css-mode" "Major mode for LESS CSS." )
-(add-to-list 'auto-mode-alist '("\\.less$" . less-css-mode))
+(add-to-list 'auto-mode-alist '("\\.less\\'" . less-css-mode))
 
 ;; Coffee-mode.
 (autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript.")
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-
-(setq-default web-mode-markup-indent-offset 2)
+(add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
 
 (provide 'mode-mappings)
