67a71ced63c5cd2225c665b04e3efe7bf821fe56
William Bert <william.bert@gmail.com>
2018-05-26 16:07:27 -0700

    Markdown use org-table.

--- setup-lisp/setup-markdown.el
+++ setup-lisp/setup-markdown.el
@@ -71,6 +71,66 @@
 (after-load 'markdown-mode
   (define-key markdown-mode-map (kbd "C-c C-b") 'browse-at-remote))
 
+;; Useful:
+;; C-c C-s C-p - `markdown-pre-region'
+;;                Indent the selected region 4 spaces to the right
+;;                (code block formatting used on reddit, stackexchange, etc.)
+
+;; from https://github.com/kaushalmodi/.emacs.d/blob/master/setup-files/setup-markdown.el
+
+(use-package markdown-mode
+  :mode (("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :config
+  (progn
+    ;; Seamless editing of Markdown tables (allowed in GFM) using `orgtbl-mode'
+    ;; http://stackoverflow.com/a/20912535/1219634
+    ;; https://gist.github.com/yryozo/5807243
+    (defun orgtbl-to-gfm (table params)
+      "Convert the Orgtbl mode TABLE to GitHub Flavored Markdown."
+      (let* ((alignment (mapconcat (lambda (x)
+                                     (if x
+                                         "|--:"
+                                       "|---"))
+                                   org-table-last-alignment ""))
+             (params2 (list :splice t
+                            :hline (concat alignment "|")
+                            :lstart "| " :lend " |" :sep " | ")))
+        (orgtbl-to-generic table (org-combine-plists params2 params))))
+    (add-hook 'markdown-mode-hook #'orgtbl-mode)
+
+    (bind-keys
+     :map markdown-mode-map
+      ;; Mimicking the org-export style bindings
+      ("C-c C-e o" . markdown-preview)
+      ("C-c C-e t". orgtbl-send-table))))
+
+;; Example orgtbl template:
+;;
+;; <!--- BEGIN RECEIVE ORGTBL foo-tbl -->
+;; | a | b |
+;; |---|---|
+;; | c | d |
+;; <!--- END RECEIVE ORGTBL foo-tbl -->
+;; <!---
+;;  - Title row is needed.
+;;  - Horizontal rule below title row is needed.
+;;  - The table identifier 'foo-tbl' after SEND has to match with that in the
+;;    BEGIN RECEIVE and END RECEIVE lines above.
+;; #+orgtbl: SEND foo-tbl orgtbl-to-gfm
+;; | a | b |
+;; |---+---|
+;; | c | d |
+;; -->
+;;
+;; 1. Paste the above template in a `markdown-mode' buffer (without the elisp
+;;    comment delimiters ";;").
+;; 2. Rename 'foo-tbl' to whatever is more appropriate (optional).
+;; 3. With point *inside* the 'SEND' table, call `orgtbl-send-table'.
+;;
+;;    Above is tested to work with `hugo' (which uses the BlackFriday markdown
+;; parser).
+
 (provide 'setup-markdown)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
