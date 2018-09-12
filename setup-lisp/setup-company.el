4fc920ba3862f3c660497cbe78432a8a8f046f97
William Bert <william.bert@gmail.com>
2018-09-12 14:30:31 -0700

    TODO

--- setup-lisp/setup-company.el
+++ setup-lisp/setup-company.el
@@ -2,13 +2,17 @@
   (add-hook 'after-init-hook 'global-company-mode)
   ;; (add-hook 'js2-mode-hook (lambda () (company-mode)))
 
+  ;; TODO:
+  ;; - group backends
+  ;; - order backends in a way that makes sense
+  (when (require 'company-emoji nil t)
+    (push 'company-emoji company-backends))
+
   (when (require 'company-lsp nil t)
     (push 'company-lsp company-backends))
 
   (when (require 'company-restclient nil t)
     (push 'company-restclient company-backends))
-
-  (when (require 'company-emoji nil t)
-    (push 'company-emoji company-backends)))
+  )
 
 (provide 'setup-company)
