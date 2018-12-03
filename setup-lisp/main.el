aeaea4ec00d1510f5e999fda71e269e187ff9e1c
William Bert <william.bert@gmail.com>
2018-12-03 14:43:28 -0800

    update whitespace characters

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -1004,6 +1004,21 @@
   :config
   (add-hook 'html-mode-hook #'lsp-html-enable))
 
+;; from https://gitlab.petton.fr/nico/emacs.d/
+(use-package whitespace
+  :config
+  (setq whitespace-display-mappings
+        '(
+          (space-mark 32 [183] [46]) ; normal space, ·
+          (space-mark 160 [164] [95])
+          (space-mark 2208 [2212] [95])
+          (space-mark 2336 [2340] [95])
+          (space-mark 3616 [3620] [95])
+          (space-mark 3872 [3876] [95])
+          (newline-mark 10 [182 10]) ; newlne, ¶
+          (tab-mark 9 [9655 9] [92 9]) ; tab, ▷
+          )))
+
 ;; Smart-tab. See: https://raw.github.com/genehack/smart-tab/master/smart-tab.el
 ;; and https://www.emacswiki.org/emacs/TabCompletion#SmartTab
 (use-package smart-tab
