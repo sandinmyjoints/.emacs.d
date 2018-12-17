fa8ad8a67ef325d9c5df7144273de4aefafc6835
William Bert <william.bert@gmail.com>
2018-12-16 22:03:57 -0800

    Add smart-jump and slime.

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -391,15 +391,27 @@
   :config
   (defalias #'runthis #'quickrun))
 
+(use-package slime)
+
 (use-package dumb-jump
   ;; :bind (("M-g o" . dumb-jump-go-other-window)
   ;;        ("M-g j" . dumb-jump-go)
   ;;        ("M-g i" . dumb-jump-go-prompt)
   ;;        ("M-g x" . dumb-jump-go-prefer-external)
   ;;        ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  ;; C-M-g dumb-jump-go
+  ;; C-M-p dumb-jump-back
   :config
   (setq dumb-jump-selector 'helm) ;; (setq dumb-jump-selector 'helm)
   (add-hook 'prog-mode-hook #'dumb-jump-mode))
+
+(use-package smart-jump
+  :config
+  ;; H-j smart-jump-go
+  ;; H-p smart-jump-back
+  (smart-jump-setup-default-registers)
+  (global-set-key (kbd "H-j") #'smart-jump-go)
+  (global-set-key (kbd "H-p") #'smart-jump-back))
 
 (require 'setup-tramp)
 
