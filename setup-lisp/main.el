edf4ea884aea073996b98b85c62a7cdf8ea57eaf
William Bert <william.bert@gmail.com>
2019-02-13 20:57:10 -0800

    helm settings

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -1097,6 +1097,14 @@
                                     helm-source-dired-recent-dirs
                                     helm-source-buffer-not-found
                                     ))
+
+  (setq
+   ;; helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+   ;; helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+   ;; helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+   ;; helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+   ;; helm-ff-file-name-history-use-recentf t
+   helm-echo-input-in-header-line t)
   )
 
 (use-package helm-org-rifle)
