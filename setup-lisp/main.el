7e51931ac76f495d18607ecb3dd75aee1c14195a
William Bert <william.bert@gmail.com>
2019-09-11 21:02:41 -0400

    Add toggle-map.

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -2878,7 +2878,59 @@ resized horizontally or vertically."
 
 (require 'appearance)
 
-(message (concat "exec-path is " (format "%s" exec-path)))
+;; From http://endlessparentheses.com/the-toggle-map-and-wizardry.html
+(define-prefix-command 'endless/toggle-map)
+;; The manual recommends C-c for user keys, but C-x t is
+;; always free, whereas C-c t is used by some modes.
+(define-key ctl-x-map "t" 'endless/toggle-map)
+(define-key endless/toggle-map "c" #'column-number-mode)
+(define-key endless/toggle-map "d" #'toggle-debug-on-error)
+(define-key endless/toggle-map "e" #'toggle-debug-on-error)
+(define-key endless/toggle-map "f" #'auto-fill-mode)
+(define-key endless/toggle-map "l" #'toggle-truncate-lines)
+(define-key endless/toggle-map "q" #'toggle-debug-on-quit)
+(define-key endless/toggle-map "t" #'endless/toggle-theme)
+(define-key endless/toggle-map "D" #'toggle-window-dedicated)
+
+;;; Generalized version of `read-only-mode'.
+(define-key endless/toggle-map "r" #'dired-toggle-read-only)
+(autoload 'dired-toggle-read-only "dired" nil t)
+(define-key endless/toggle-map "w" #'whitespace-mode)
+
+;; From http://endlessparentheses.com/emacs-narrow-or-widen-dwim.html
+(defun narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+Dwim means: region, org-src-block, org-subtree, or
+defun, whichever applies first. Narrowing to
+org-src-block actually calls `org-edit-src-code'.
+
+With prefix P, don't widen, just narrow even if buffer
+is already narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning)
+                           (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing
+         ;; command. Remove this first conditional if
+         ;; you don't want it.
+         (cond ((ignore-errors (org-edit-src-code) t)
+                (delete-other-windows))
+               ((ignore-errors (org-narrow-to-block) t))
+               (t (org-narrow-to-subtree))))
+        ((derived-mode-p 'latex-mode)
+         (LaTeX-narrow-to-environment))
+        (t (narrow-to-defun))))
+
+(define-key endless/toggle-map "n"
+  #'narrow-or-widen-dwim)
+
+;; This line actually replaces Emacs' entire narrowing
+;; keymap, that's how much I like this command. Only
+;; copy it if that's what you want.
+(define-key ctl-x-map "n" #'narrow-or-widen-dwim)
 
 (defvar initial-file (expand-file-name "init.el" user-emacs-directory))
 
