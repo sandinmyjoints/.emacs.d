daaddda9b9586b8d2801be0f02b72d0c2cc57cf5
William Bert <william.bert@gmail.com>
2019-09-16 11:59:01 -0400

    remove unneeded code

--- setup-lisp/setup-dirtree.el
+++ setup-lisp/setup-dirtree.el
@@ -100,13 +100,7 @@
   (force-mode-line-update)
   ;; Apparently force-mode-line-update is not always enough to
   ;; redisplay the mode-line
-  (redraw-display)
-  (when (and (called-interactively-p 'interactive)
-             hidden-mode-line-mode)
-    (run-with-idle-timer
-     0 nil 'message
-     (concat "Hidden Mode Line Mode enabled.  "
-             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+  (redraw-display))
 
 (add-hook 'dirtree-mode-hook 'hidden-mode-line-mode)
 
