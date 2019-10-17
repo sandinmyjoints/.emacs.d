f13b8e49c6435d610ea9eeb4f84ee2afb187265b
William Bert <william.bert@gmail.com>
2019-10-17 16:08:10 -0400

    working camelscore-word-at-point

--- setup-lisp/defuns.el
+++ setup-lisp/defuns.el
@@ -392,6 +392,35 @@
 
 (defalias #'camelize-thing-at-point #'wjb/camelize-thing-at-point)
 
+;; Working:
+(defun split-name (s)
+  (split-string
+   (let ((case-fold-search nil))
+     (downcase
+      (replace-regexp-in-string "\\([a-z]\\)\\([A-Z]\\)" "\\1 \\2" s)))
+   "[^A-Za-z0-9]+"))
+
+(defun camelcase  (s) (mapconcat 'capitalize (split-name s) ""))
+(defun underscore (s) (mapconcat 'downcase   (split-name s) "_"))
+(defun dasherize  (s) (mapconcat 'downcase   (split-name s) "-"))
+(defun colonize   (s) (mapconcat 'capitalize (split-name s) "::"))
+
+(defun camelscore (s)
+  (cond ((string-match-p "\:"  s)	(camelcase s))
+        ((string-match-p "-" s)     (colonize s))
+        ((string-match-p "_" s)	(dasherize s))
+        (t   (underscore s)) ))
+
+;; Entrypoint:
+(defun camelscore-word-at-point ()
+  (interactive)
+  (let* ((case-fold-search nil)
+         (beg (and (skip-chars-backward "[:alnum:]:_-") (point)))
+         (end (and (skip-chars-forward  "[:alnum:]:_-") (point)))
+         (txt (buffer-substring beg end))
+         (cml (camelscore txt)) )
+    (if cml (progn (delete-region beg end) (insert cml))) ))
+
 ;; TODO: camelize-all-like-thing-at-point
 
 
