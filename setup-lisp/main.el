b7a95e585dabc6c73df8592d9f1679201fce783d
William Bert <william.bert@gmail.com>
2019-10-17 16:08:50 -0400

    use treemacs instead of dirtree

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -1641,12 +1641,12 @@ If PROJECT is not specified the command acts on the current project."
   (global-set-key (kbd "C-c w s") 'copy-as-format-slack)
   (global-set-key (kbd "C-c w g") 'copy-as-format-github))
 
-(require 'setup-dirtree)
-(with-eval-after-load 'dirtree
-  (progn
-    ;; Free up for helm-mini.
-    (unbind-key (kbd "C-o")  dirtree-mode-map)
-    (bind-key (kbd "<return>") 'dirtree-display dirtree-mode-map)))
+;; (require 'setup-dirtree)
+;; (with-eval-after-load 'dirtree
+;;   (progn
+;;     ;; Free up for helm-mini.
+;;     (unbind-key (kbd "C-o")  dirtree-mode-map)
+;;     (bind-key (kbd "<return>") 'dirtree-display dirtree-mode-map)))
 
 ;; To prevent opening stuff from dirtree from splitting the one reusable window that I use:
 ;; From https://www.reddit.com/r/emacs/comments/80pd2q/anyone_could_help_me_with_window_management/dux9cme/
@@ -1661,6 +1661,97 @@ If PROJECT is not specified the command acts on the current project."
                                          '((inhibit-same-window . nil)
                                            (inhibit-switch-frame . t))))
         ))
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 3
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-follow-delay             0.2
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil ;; TODO t
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    'on-distance
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-desc
+          treemacs-space-between-root-nodes      nil
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          ;; 55 is good for widescreen. (treemacs-set-width 45) is good for laptop.
+          treemacs-width                         55)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    (treemacs-resize-icons 18)
+
+    ;; TODO cursor-type box in hook
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (treemacs-git-mode -1)
+    (add-hook 'treemacs-mode-hook 'hidden-mode-line-mode)
+    (set-face-attribute 'treemacs-root-face nil :height 1.0 :underline nil)
+    )
+
+  (defun wjb/treemacs-ignore-compiled-files (filename filepath)
+    (or
+     (s-equals? (file-name-extension filename) "elc")
+     (s-equals? (file-name-extension filename) "pyc")))
+  (push #'wjb/treemacs-ignore-compiled-files treemacs-ignored-file-predicates)
+
+  :bind
+  (:map global-map
+        ("H-a"       . treemacs-select-window)
+        ("C-c d"     . treemacs-add-project)
+        ;; ("C-x t 1"   . treemacs-delete-other-windows)
+        ;; ("C-x t t"   . treemacs)
+        ;; ("C-x t B"   . treemacs-bookmark)
+        ;; ("C-x t C-t" . treemacs-find-file)
+        ;; ("C-x t M-t" . treemacs-find-tag)
+        )
+  (:map treemacs-mode-map
+        ("e" . treemacs-TAB-action)
+        ("j" . treemacs-next-line)
+        ("k" . treemacs-previous-line)
+        )
+  )
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+;; (use-package treemacs-icons-dired
+;;   :after treemacs dired
+;;   :ensure t
+;;   :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :disabled
+  :ensure t)
 
 ;; (require 'lisp-stuff)
 
