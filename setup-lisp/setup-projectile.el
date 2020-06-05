07370f59b35bf720ee007a602b16ba6eb5876d34
William Bert <william.bert@gmail.com>
2020-06-05 08:27:59 -0400

    Define projectile project types better.

--- setup-lisp/setup-projectile.el
+++ setup-lisp/setup-projectile.el
@@ -43,6 +43,7 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
+;; Package-Requires: ((emacs "25.1"))
 ;;; Code:
 
 ;; projectile-mode keymap
@@ -93,7 +94,25 @@
 ;;
 (eval-after-load 'projectile-mode
   ;; projectile-find-file-dwim is more generalized than projectile-find-file
-  (define-key projectile-mode-map (kbd "C-c p g") 'projectile-find-file-dwim))
+  (progn
+    (define-key projectile-command-map (kbd "g") 'projectile-find-file-dwim)
+    (define-key projectile-command-map (kbd "a") 'projectile-find-related-file)))
+
+
+
+
+;; Project types.
+
+;; Check project type with:
+;; M-: (projectile-project-type)
+
+;; How to specify project-type using dir-locals:
+;; ((nil . ((projectile-project-type . use-async-queue))))
+
+;; Reset project types, good for testing:
+(defun wjb/reset-projectile-project-types ()
+  (interactive)
+  (setq projectile-project-types '()))
 
 
 ;; auth
@@ -133,7 +152,7 @@
 ;; playground
 ;; src/controller/file.js
 ;; test/controller/file.test.js
-;; test/controller/file.db.test.js
+;; test/controller/file.db.test.js -- not handled yet
 (defun wjb/related-files-corresponding-path-playground (path)
   (if (string-match (rx (group (or "src" "test"))
                         (group "/" (+? anything))
@@ -154,10 +173,98 @@
 ;; neodarwin server
 ;; test/file.coffee but for controllers there's common|desktop|mobile
 
+(defun projectile-find-impl-file ()
+  "Open impl file."
+  (interactive)
+  (find-file
+   (projectile--find-related-file (buffer-file-name) :impl)))
+
+(defun projectile-find-test-file ()
+  "Open test file."
+  (interactive)
+  (find-file
+   (projectile--find-related-file (buffer-file-name) :test)))
+
+(defun projectile-find-stories-file ()
+  "Open stories file."
+  (interactive)
+  (find-file
+   (projectile--find-related-file (buffer-file-name) :stories)))
+
+(defun projectile-find-translations-file ()
+  "Open translations file."
+  (interactive)
+  (find-file
+   (projectile--find-related-file (buffer-file-name) :translations)))
+
+(defun projectile-find-styles-file ()
+  "Open styles file."
+  (interactive)
+  (find-file
+   (projectile--find-related-file (buffer-file-name) :styles)))
+
+;; Can also use H-p for C-c p
+;; C-c p s o stories
+;; C-c p s y styles
+;; C-c p s t tests
+;; C-c p s r translations
+;; C-c p s i impl/index
+(eval-after-load 'projectile-mode
+  ;; projectile-find-file-dwim is more generalized than projectile-find-file
+  (progn
+    (define-key projectile-command-map (kbd "s i") 'projectile-find-impl-file)
+    (define-key projectile-command-map (kbd "s t") 'projectile-find-test-file)
+    (define-key projectile-command-map (kbd "s o") 'projectile-find-stories-file)
+    (define-key projectile-command-map (kbd "s y") 'projectile-find-styles-file)
+    (define-key projectile-command-map (kbd "s r") 'projectile-find-translations-file)
+    ))
+
+
 ;; neodarwin components
 ;; src/components/test.js
 ;; src/components/test.jsx
 (defun wjb/related-files-same-dir-components (path)
+  (if (string-match (rx (group (+? anything))
+                        (group (1+ (not (any "/"))) (or ".js" ".jsx" ".less"))) path)
+      (let* ((dir (match-string 1 path))
+             (filename (match-string 2 path))
+             (filename-base (file-name-base filename))
+             (filename-extension (file-name-extension filename t))
+             (filename-extension-usable (cond
+                                         ((equal filename-base "translations") ".jsx")
+                                         ((equal filename-base "stories") ".jsx")
+                                         ((equal filename-extension ".less") ".jsx")
+                                         (t filename-extension)))
+             (filename-impl (concat "index" filename-extension-usable))
+             (filename-test (concat "test" filename-extension-usable))
+             (filename-translations "translations.js")
+             (filename-styles "styles.less")
+             (filename-stories "stories.jsx"))
+
+        ;; non-destructive
+        ;; (setq wjb/tt (list :a 1 :b 2))
+        ;; (org-plist-delete wjb/tt :a)
+
+        (let ((relations (list :impl (concat dir filename-impl)
+                               :test (concat dir filename-test)
+                               :translations (concat dir filename-translations)
+                               :styles (concat dir filename-styles)
+                               :stories (concat dir filename-stories))))
+          (cond
+           ((equal filename-base "test")
+            (org-plist-delete relations :test))
+           ((equal filename-base "translations")
+            (org-plist-delete relations :translations))
+           ((equal filename-base "index")
+            (org-plist-delete relations :impl))
+           ((equal filename-base "stories")
+            (org-plist-delete relations :stories))
+           ((equal filename-base "styles")
+            (org-plist-delete relations :styles))
+           (t relations
+            ))))))
+
+(defun wjb/related-files-same-dir-components-orig (path)
   (if (string-match (rx (group (+? anything))
                         (group (1+ (not (any "/"))) (or ".js" ".jsx"))) path)
       (let* ((dir (match-string 1 path))
@@ -173,6 +280,43 @@
             (list :impl (concat dir filename-impl))
           (list :test (concat dir filename-test))))))
 
+;; use-async-queue
+;; pairs are in same directory:
+;; use.js
+;; use.test.js
+(projectile-register-project-type
+ 'use-async-queue '("use-async-queue.js")
+ :configure "yarn"
+ :compile "yarn build"
+ :test "yarn test"
+ :src-dir "."
+ :test-dir "."
+ :test-suffix ".test")
+
+
+(projectile-register-project-type 'neodarwin '("use-dir-locals")
+				  :configure "yarn"
+          :compile "yarn run watch:build:dev"
+				  :test "npm test"
+          :src-dir "src"
+				  :test-dir "test"
+          :related-files-fn (list (projectile-related-files-fn-test-with-suffix "js" ".test")
+                                  (projectile-related-files-fn-test-with-suffix "jsx" ".test")
+                                  #'wjb/related-files-same-dir-components
+                                  #'wjb/related-files-corresponding-path-po-intake
+                                  #'wjb/related-files-corresponding-path)
+          )
+
+;; separate src and test
+;; sd-auth, sd-playground, atalanta, neodarwin server (except for db test)
+(projectile-register-project-type
+ 'sd-auth '("use-dir-locals")
+ :configure "yarn"
+ :test "yarn test"
+ :run "yarn start"
+ :related-files-fn #'wjb/related-files-corresponding-path)
+
+;; Generic fallback
 (projectile-register-project-type 'npm '("package.json")
 				  :compile "npm install"
 				  :test "npm test"
@@ -182,20 +326,50 @@
           :related-files-fn (list (projectile-related-files-fn-test-with-suffix "js" "test")
                                   (projectile-related-files-fn-test-with-suffix "jsx" "test")
                                   (projectile-related-files-fn-test-with-suffix "js" "test.js")
-                                  (projectile-related-files-fn-test-with-suffix "js" "db.test.js")
                                   #'wjb/related-files-corresponding-path-playground
+                                  (projectile-related-files-fn-test-with-suffix "js" "db.test.js")
                                   #'wjb/related-files-same-dir-components
                                   #'wjb/related-files-corresponding-path-po-intake
                                   #'wjb/related-files-corresponding-path)
           )
 
-;; (projectile-register-project-type 'yarn '("yarn.lock")
-;; 				  :compile "yarn"
-;; 				  :test "npm test"
-;; 				  :run "npm start"
-;;           :src-dir "src"
-;; 				  :test-dir "test"
-;;           )
+;; Generic, for when source and test files are in same dir.
+;; More generic should come later.
+;; use-async-queue, sort of equivalency
+(projectile-register-project-type
+ 'yarn-same-dir '("yarn.lock")
+ :configure "yarn"
+ :test "yarn test"
+ :run "yarn start"
+ :test-suffix ".test")
+
+(projectile-register-project-type
+ 'po-intake '("set-with-dir-locals")
+ :configure "yarn"
+ :test "yarn test"
+ :run "yarn start"
+ :src-dir "src"
+ :test-dir "test/server"
+ :test-suffix ".test")
+
+;; a.el and a_test.el
+;; ext is el
+;; suffix is _test
+;; a.js and a.db.test.js
+;; ext is js
+;; suffix is .db.test, or
+;; suffix is db.test.
+;; suffix is .db.test.
+(projectile-register-project-type
+ 'sd-playground '("set-with-dir-locals")
+ :configure "yarn"
+ :test "yarn test"
+ :run "yarn start"
+ :src-dir "src"
+ :test-dir "test"
+ :related-files-fn (list (projectile-related-files-fn-test-with-suffix "js" ".test")
+                         ;; this seems like it should work, but it does not:
+                         (projectile-related-files-fn-test-with-suffix "js" ".db.test")))
 
 (provide 'setup-projectile)
 
