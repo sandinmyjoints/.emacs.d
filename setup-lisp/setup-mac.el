2f5b2af61a26b49e8d66dcad069ebb69252183b3
William Bert <william.bert@gmail.com>
2022-06-20 11:46:59 -0400

    update setup-mac

--- setup-lisp/setup-mac.el
+++ setup-lisp/setup-mac.el
@@ -109,6 +109,20 @@
   :mode "\\.plist\\'"
   :ensure t)
 
+;; Allow editing of binary .plist files.
+(add-to-list 'jka-compr-compression-info-list
+             ["\\.plist$"
+              "converting text XML to binary plist"
+              "plutil"
+              ("-convert" "binary1" "-o" "-" "-")
+              "converting binary plist to text XML"
+              "plutil"
+              ("-convert" "xml1" "-o" "-" "-")
+              nil nil "bplist"])
+
+;;It is necessary to perform an update!
+(jka-compr-update)
+
 (provide 'setup-mac)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
