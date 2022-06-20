7bf91234303c1053360fd70a8648f1d175833cb2
William Bert <william.bert@gmail.com>
2022-06-20 11:51:07 -0400

    update dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((nil
-  (find-in-project-dir . "~/.emacs.d")))
+  (find-in-project-dir . "~/.emacs.d")
+  ;; (eval . (progn
+  ;;           (treemacs-do-switch-workspace
+  ;;            (treemacs--select-workspace-by-name "Default"))))
+  ))
