37bfbae4fa189fdf77d96acfddf6f11bc213f253
William Bert <william.bert@gmail.com>
2022-07-30 15:04:43 -0400

    add rustic

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -2789,6 +2789,31 @@ If PROJECT is not specified the command acts on the current project."
   (global-set-key (kbd "C-c ~") #'vimish-fold))
 
 
+;; rust
+
+;; very helpful guide: https://robert.kra.hn/posts/rust-emacs-setup/
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  )
+
+
 ;; js
 
 (use-package indium
