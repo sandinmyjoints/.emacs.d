c8a6dd8ae190fa07567da912268b0ee55d177dfd
William Bert <william.bert@gmail.com>
2023-02-08 16:15:39 -0500

    Organize python config a bit.

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -1167,11 +1167,8 @@ Fix for the above hasn't been released as of Emacs 25.2."
   (elpy-enable)
   (setq elpy-modules (-remove-item 'elpy-module-flymake elpy-modules)))
 
-(use-package smart-dash
-  :disabled
-  :hook (python-mode . smart-dash-mode))
-
 (use-package pyvenv
+  :disabled
   :defer t
   :config
   (setenv "WORKON_HOME" (expand-file-name "~/.local/venvs/"))
@@ -1222,7 +1219,7 @@ Fix for the above hasn't been released as of Emacs 25.2."
     (if (executable-find "ipython")
         (setq
          python-shell-interpreter "ipython"
-         ;;python-shell-interpreter-args "--no-banner --gui=osx"
+         ;; python-shell-interpreter-args "--no-banner --gui=osx"
          python-shell-prompt-regexp "In \\[[0-9]+\\]: "
          python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
          python-shell-completion-setup-code
@@ -1231,29 +1228,26 @@ Fix for the above hasn't been released as of Emacs 25.2."
          "';'.join(module_completion('''%s'''))\n"
          python-shell-completion-string-code
          "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-      (setq
-       python-shell-interpreter "python"
-       python-shell-interpreter-args "-i"
-       python-shell-prompt-regexp ">>> "
-       python-shell-prompt-output-regexp ""
-       python-shell-completion-setup-code
-       "try:\n    import readline\nexcept ImportError:\n    def __COMPLETER_all_completions(text): []\nelse:\n    import rlcompleter\n    readline.set_completer(rlcompleter.Completer().complete)\n    def __COMPLETER_all_completions(text):\n        import sys\n        completions = []\n        try:\n            i = 0\n            while True:\n                res = readline.get_completer()(text, i)\n                if not res: break\n                i += 1\n                completions.append(res)\n        except NameError:\n            pass\n        return completions"
-       python-shell-completion-module-string-code ""
-       python-shell-completion-string-code "';'.join(__COMPLETER_all_completions('''%s'''))"
+      (setq python-shell-interpreter "python"
+            python-shell-interpreter-args "-i"
+            python-shell-prompt-regexp ">>> "
+            python-shell-prompt-output-regexp ""
+            python-shell-completion-setup-code
+            "try:\n    import readline\nexcept ImportError:\n    def __COMPLETER_all_completions(text): []\nelse:\n    import rlcompleter\n    readline.set_completer(rlcompleter.Completer().complete)\n    def __COMPLETER_all_completions(text):\n        import sys\n        completions = []\n        try:\n            i = 0\n            while True:\n                res = readline.get_completer()(text, i)\n                if not res: break\n                i += 1\n                completions.append(res)\n        except NameError:\n            pass\n        return completions"
+            python-shell-completion-module-string-code ""
+            python-shell-completion-string-code "';'.join(__COMPLETER_all_completions('''%s'''))"
        )))
   (ad-activate 'run-python)
-
-  ;; from https://emacs.stackexchange.com/a/30970/2163
-  ;; (with-eval-after-load 'python
-  ;;   (defun python-shell-completion-native-try ()
-  ;;     "Return non-nil if can trigger native completion."
-  ;;     (let ((python-shell-completion-native-enable t)
-  ;;           (python-shell-completion-native-output-timeout
-  ;;            python-shell-completion-native-try-output-timeout))
-  ;;       (python-shell-completion-native-get-completions
-  ;;        (get-buffer-process (current-buffer))
-  ;;        nil "_"))))
   )
+
+(use-package pip-requirements
+  :mode
+  "requirements\\.txt"
+  "requirements\\.*\\.txt")
+
+(use-package smart-dash
+  :disabled
+  :hook (python-mode . smart-dash-mode))
 
 (use-package ein
   :after python
@@ -1263,11 +1257,6 @@ Fix for the above hasn't been released as of Emacs 25.2."
         ("C-c C-g" . 'ein:notebooklist-open))
   (:map ein:notebook-mode-map
         ("C-c C-g" . 'ein:notebooklist-open)))
-
-(use-package pip-requirements
-  :mode
-  "requirements\\.txt"
-  "requirements\\.*\\.txt")
 
 
 ;; projectile
