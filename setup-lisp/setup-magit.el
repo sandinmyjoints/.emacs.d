99f7cfe3a97a30b5c0c039f0783b6aaa1bd008ba
William Bert <william.bert@gmail.com>
2023-03-30 16:17:46 -0400

    Delete old magit config code.

--- setup-lisp/setup-magit.el
+++ setup-lisp/setup-magit.el
@@ -2,24 +2,6 @@
 
 ;;; Code:
 
-;;(when (require 'magit nil t))
-
-;; Old magit diff colors.
-;; '(progn
-;;    ;; Use list-faces-display to examine all faces.
-;;    (set-face-foreground 'magit-diff-add "green3") ;; Normal by default.
-;;    (set-face-foreground 'magit-diff-del "red3") ;; Normal by default.
-;;    (set-face-background 'magit-diff-add "black") ;; Dull green by default.
-;;    (set-face-background 'magit-diff-del "black") ;; Dull red by default.
-;;    (set-face-background 'magit-item-highlight "#101212")
-;;    (set-face-background 'magit-diff-none "black") ;; Black by default.
-;;    (unless (display-graphic-p)
-;;      (set-face-background 'magit-item-highlight "black"))
-
-;;    (set-face-attribute 'magit-item-highlight nil
-;;                        :weight 'normal))
-
-;; New magit diff colors:
 (set-face-bold 'magit-diff-file-heading nil)
 ;; (set-face-background 'magit-diff-context-highlight "grey10")
 ;; (set-face-background 'magit-diff-context "grey10")
