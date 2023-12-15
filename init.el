fb195e3bb3d6266c85d52d24fdb8d3cb1792a97b
William Bert <william.bert@gmail.com>
2023-12-15 14:59:00 -0500

    Set high gc threshold.

--- init.el
+++ init.el
@@ -47,6 +47,7 @@
 
 ;;(add-to-list 'load-path (expand-file-name "elisp/gcmh" user-emacs-directory))
 ;;(gcmh-mode 1)
+(setq gc-cons-threshold (* 100 1000 1000))
 
 ;; (setq debug-on-error t)
 (defun init ()
