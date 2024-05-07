0b856d4238ec94d0bb4bba049c5a9ad67ae1d2a1
William Bert <william.bert@gmail.com>
2024-05-07 16:09:48 -0400

    Combobulate (disabled).

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -3881,6 +3881,27 @@ questions.  Else use completion to select the tab to switch to."
   (treesit-auto-add-to-auto-mode-alist treesit-auto-langs)
   (global-treesit-auto-mode))
 
+(use-package combobulate
+  :disabled
+  :preface
+  (setq combobulate-key-prefix "C-c o")
+
+  ;; Optional, but recommended.
+  ;;
+  ;; You can manually enable Combobulate with `M-x
+  ;; combobulate-mode'.
+  ;; :hook
+  ;;   ((python-ts-mode . combobulate-mode)
+  ;;    (js-ts-mode . combobulate-mode)
+  ;;    (html-ts-mode . combobulate-mode)
+  ;;    (css-ts-mode . combobulate-mode)
+  ;;    (yaml-ts-mode . combobulate-mode)
+  ;;    (typescript-ts-mode . combobulate-mode)
+  ;;    (json-ts-mode . combobulate-mode)
+  ;;    (tsx-ts-mode . combobulate-mode))
+  ;; Amend this to the directory where you keep Combobulate's source
+  ;; code.
+  :load-path ("elisp/combobulate"))
 
 
 ;; key-bindings. Must be after defuns. Should be near the end, to avoid being overwritten.
