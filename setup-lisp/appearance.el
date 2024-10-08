3173f839c06bbd34029a9beabe1238684d786f4c
William Bert <william.bert@gmail.com>
2024-10-08 15:47:47 -0400

    see menu bar

--- setup-lisp/appearance.el
+++ setup-lisp/appearance.el
@@ -53,7 +53,7 @@
 ;;
 (if (display-graphic-p)
     (progn
-      (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+      ;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
       (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
       (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
       (if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))))
