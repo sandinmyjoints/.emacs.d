02777bb5f181e4137823963171e230a1a8dde54d
William Bert <william.bert@gmail.com>
2025-01-09 17:08:16 -0500

    helpful

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -2418,34 +2418,16 @@ Insert .* between each char."
   :diminish visual-line-mode)
 
 (use-package helpful
-  :defer
-  :after help-mode
+  :bind
+  (("C-h f" . helpful-function)
+   ("C-h x" . helpful-command)
+   ("C-h k" . helpful-key)
+   ("C-h v" . helpful-variable))
   :config
   (add-hook 'helpful-mode-hook 'visual-line-mode)
-
-  ;; Note that the built-in `describe-function' includes both functions
-  ;; and macros. `helpful-function' is functions only, so we provide
-  ;; `helpful-callable' as a drop-in replacement.
-  (global-set-key (kbd "C-h f") #'helpful-callable)
-  (global-set-key (kbd "C-h v") #'helpful-variable)
-  (global-set-key (kbd "C-h k") #'helpful-key)
-  (global-set-key (kbd "C-c C-d") #'helpful-at-point)
   ;; Lookup the current symbol at point. C-c C-d is a common keybinding
   ;; for this in lisp modes.
-  (global-set-key (kbd "C-c C-d") #'helpful-at-point)
-
-  ;; Look up *F*unctions (excludes macros).
-  ;;
-  ;; By default, C-h F is bound to `Info-goto-emacs-command-node'. Helpful
-  ;; already links to the manual, if a function is referenced there.
-  (global-set-key (kbd "C-h F") #'helpful-function)
-
-  ;; Look up *C*ommands.
-  ;;
-  ;; By default, C-h C is bound to describe `describe-coding-system'. I
-  ;; don't find this very useful, but it's frequently useful to only
-  ;; look at interactive functions.
-  (global-set-key (kbd "C-h C") #'helpful-command))
+  (global-set-key (kbd "C-c C-d") #'helpful-at-point))
 
 ;; between which-key and which-key-posframe, they are making typing and
 ;; navigation within buffers slow
