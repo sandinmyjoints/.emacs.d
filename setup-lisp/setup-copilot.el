d820615ecec15a9c38052a9747d522832d053592
William Bert <william.bert@gmail.com>
2025-03-04 12:05:21 -0500

    update copilot config

--- setup-lisp/setup-copilot.el
+++ setup-lisp/setup-copilot.el
@@ -41,8 +41,11 @@ cleared, make sure the overlay doesn't come back too soon."
              (setq copilot-disable-predicates pre-copilot-disable-predicates)))))
     (error handler)))
 
+;; (defun rk/copilot-complete-if-active (next-func n)
+;;   (let ((completed (when copilot-mode (copilot-accept-completion))))
+;;     (unless completed (funcall next-func n))))
 (defun rk/copilot-complete-if-active (next-func n)
-  (let ((completed (when copilot-mode (copilot-accept-completion))))
+  (let ((completed (copilot-accept-completion)))
     (unless completed (funcall next-func n))))
 
 (defun rk/no-copilot-mode ()
@@ -57,11 +60,12 @@ cleared, make sure the overlay doesn't come back too soon."
                               comint-mode
                               compilation-mode
                               debugger-mode
-                              dired-mode-hook
-                              compilation-mode-hook
-                              flutter-mode-hook
+                              dired-mode
+                              compilation-mode
+                              flutter-mode
                               jest-mode
-                              minibuffer-mode-hook)
+                              text-mode
+                              minibuffer-mode)
   "Modes in which copilot is inconvenient.")
 
 
@@ -113,16 +117,14 @@ annoying, sometimes be useful, that's why this can be handly."
   :load-path ("elisp/copilot.el")
   :diminish
   :config
-  (setq copilot-indent-warning-suppress t)
+  ;; (setq copilot-indent-warning-suppress t)
   (setq copilot-enable-predicates '(copilot--buffer-changed))
 
-  ;; keybindings that are active when copilot shows completions
   (define-key copilot-mode-map (kbd "M-C-<next>") #'copilot-next-completion)
   (define-key copilot-mode-map (kbd "M-C-<prior>") #'copilot-previous-completion)
   (define-key copilot-mode-map (kbd "M-C-<right>") #'copilot-accept-completion-by-word)
   (define-key copilot-mode-map (kbd "M-C-<down>") #'copilot-accept-completion-by-line)
 
-  ;; global keybindings
   (define-key global-map (kbd "C-c <TAB>") #'rk/copilot-complete-or-accept)
   ;; (define-key global-map (kbd "M-C-<escape>") #'rk/copilot-change-activation)
 
@@ -136,11 +138,14 @@ annoying, sometimes be useful, that's why this can be handly."
 
   ;; deactivate copilot for certain modes
   (add-to-list 'copilot-enable-predicates #'rk/copilot-enable-predicate)
-  (add-to-list 'copilot-disable-predicates #'rk/copilot-disable-predicate))
+  (add-to-list 'copilot-disable-predicates #'rk/copilot-disable-predicate)
+  (global-copilot-mode))
 
 (use-package copilot-chat
   :config
   (define-key global-map (kbd "C-c o") #'copilot-chat-transient)
-  (setq copilot-chat-frontend 'shell-maker))
+  (setq copilot-chat-frontend 'org))
+
+(setq copilot-chat-org-prompt "You are an experienced software engineer who has worked extensively with Javascript, Typescript, React, Node, MySQL, Python, Django, Bash. Please minimize explanations and assume a high level of knowledge. When asked for your name, you must respond with \"GitHub Copilot\".\nFollow the user's requirements carefully & to the letter.\nYour expertise is strictly limited to software development topics.\nAvoid content that violates copyrights.\nFor questions not related to software development, simply give a reminder that you are an AI programming assistant.\nKeep your answers short and impersonal.\n\nUse only Emacs org-mode formatting in your answers.\nWhen using heading to structure your answer, please start at level 3 (i.e with 3 stars or more)\nMake sure to include the programming language name at the start of the org-mode code blocks.\nThis is an example of python code block in emacs org-mode syntax:\n#+BEGIN_SRC python\ndef hello_world():\n	print('Hello, World!')\n#+END_SRC\nAvoid wrapping the whole response in the block code.\n\nDon't forget the most important rule when you are formatting your response: use emacs org-mode syntax only.\n\nThe user works in an IDE called Emacs which has a concept for editors with open files, integrated unit test support, an output pane that shows the output of running the code as well as an integrated terminal.\nThe active document is the source code the user is looking at right now.\nYou can only give one reply for each conversation turn.\n\nAdditional Rules\nThink step by step:\n1. Examine the provided code selection and any other context like user question, related errors, project details, class definitions, etc.\n2. If you are unsure about the code, concepts, or the user's question, ask clarifying questions.\n3. If the user provided a specific question or error, answer it based on the selected code and additional provided context. Otherwise focus on explaining the selected code.\n4. Provide suggestions if you see opportunities to improve code readability, performance, etc.\n\nFocus on being clear, helpful, and thorough.\nUse developer-friendly terms and analogies in your explanations.\nIdentify 'gotchas' or less obvious parts of the code that might trip up a developer.\nProvide clear and relevant examples aligned with any provided context.\n")
 
 (provide 'setup-copilot)
