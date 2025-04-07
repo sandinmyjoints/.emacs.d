81f3c8967fe4ff89c4c98acd165caef97abc4cde
William Bert <william.bert@gmail.com>
2025-04-07 11:33:25 -0400

    Update setup-aider.

--- setup-lisp/setup-aider.el
+++ setup-lisp/setup-aider.el
@@ -12,17 +12,22 @@ and \"apikey\" as USER."
       (if (functionp secret)
           (encode-coding-string (funcall secret) 'utf-8)
         secret)
-    (user-error "No `whatever-key' found in the auth source")))
+    (user-error (format "No %s %s found in the auth source" host (or user "apikey")))))
+
+;; helpful:
+;; (auth-source-forget-all-cached)
+
+;; (auth-source-search
+;;  :max 3
+;;  :host "api.openai.com"
+;;  :user (or "apikey-aider" "apikey")
+;;  :require '(:secret))
 
 (use-package aider
   :load-path ("elisp/aider.el")
   :config
   ;; Aider wants chat models (not coder/FIM).
   (define-key global-map (kbd "C-c a") #'aider-transient-menu)
-
-  ;; (setenv "OPENAI_API_KEY" (api-key-from-auth-source "api.openai.com" "apikey-aider"))
-  (setq openai-apikey-aider (api-key-from-auth-source "api.openai.com" "apikey-aider"))
-  ;; (setq aider-args `("--config" ,(expand-file-name "~/.aider.conf.yml")))
 
   ;; https://github.com/Aider-AI/aider/blob/main/aider/resources/model-settings.yml
   ;; See ~/.aider.conf.yml
@@ -37,11 +42,15 @@ and \"apikey\" as USER."
   ;;                    "--reasoning-effort" "high"))
 
   ;; Github.
+  ;; (setq aider-program "uv"
+  ;;       aider-args
+  ;;       `("run" "aider" "--model" "github_copilot/o3-mini"
+  ;;         "--weak-model" "github_copilot/gpt-4o-mini"
+  ;;         "--editor-model" "github_copilot/gpt-4o"))
+
+  ;; can use /opt/homebrew/bin/aider in .dir-locals.el
   (setq aider-program "uv"
-        aider-args
-        `("run" "aider" "--model" "github_copilot/o3-mini"
-          "--weak-model" "github_copilot/gpt-4o-mini"
-          "--editor-model" "github_copilot/gpt-4o"))
+        aider-args '("run" "aider"))
 
   ;; Local models.
   ;; (setq aider-args '("--model" "ollama_chat/hf.co/sm54/Mistral-Small-24B-Instruct-2501-Q4_K_M-GGUF:latest"
@@ -51,6 +60,11 @@ and \"apikey\" as USER."
   ;;                    "--edit-format" "diff"
   ;;                    "--editor-model" "ollama_chat/hf.co/sm54/Mistral-Small-24B-Instruct-2501-Q4_K_M-GGUF:latest"
   ;;                    "--editor-edit-format" "editor-diff"))
+
+  ;; (auth-source-forget-all-cached)
+  ;; (setenv "OPENAI_API_KEY" (api-key-from-auth-source "api.openai.com" "apikey-aider"))
+  (setq openai-apikey-aider (api-key-from-auth-source "api.openai.com" "apikey-aider"))
+  ;; (setq aider-args `("--config" ,(expand-file-name "~/.aider.conf.yml")))
 
   )
 
