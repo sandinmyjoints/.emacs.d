4afdd8d8fac5008f3f26dc70b60d749e56485f9c
William Bert <william.bert@gmail.com>
2025-09-30 09:51:41 -0400

    Update combobulate config.

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -4012,25 +4012,22 @@ questions.  Else use completion to select the tab to switch to."
   (global-treesit-auto-mode))
 
 (use-package combobulate
-  :disabled
+  ;; :disabled
   :preface
-  (setq combobulate-key-prefix "C-c o")
+  ;; (setq combobulate-key-prefix "C-c .")
+  (setq combobulate-key-prefix "M-c") ;; or M-j
+  :config
+  (setf (cadr (assoc 'javascript combobulate-registered-languages-alist))
+        (append (cadr (assoc 'javascript combobulate-registered-languages-alist))
+                '(jtsx-jsx-mode)))
+  (setf (cadr (assoc 'tsx combobulate-registered-languages-alist))
+        (append (cadr (assoc 'tsx combobulate-registered-languages-alist))
+                '(jtsx-tsx-mode)))
+  (setf (cadr (assoc 'typescript combobulate-registered-languages-alist))
+        (append (cadr (assoc 'typescript combobulate-registered-languages-alist))
+                '(jtsx-typescript-mode)))
 
-  ;; Optional, but recommended.
-  ;;
-  ;; You can manually enable Combobulate with `M-x
-  ;; combobulate-mode'.
-  ;; :hook
-  ;;   ((python-ts-mode . combobulate-mode)
-  ;;    (js-ts-mode . combobulate-mode)
-  ;;    (html-ts-mode . combobulate-mode)
-  ;;    (css-ts-mode . combobulate-mode)
-  ;;    (yaml-ts-mode . combobulate-mode)
-  ;;    (typescript-ts-mode . combobulate-mode)
-  ;;    (json-ts-mode . combobulate-mode)
-  ;;    (tsx-ts-mode . combobulate-mode))
-  ;; Amend this to the directory where you keep Combobulate's source
-  ;; code.
+  :hook ((prog-mode . combobulate-mode))
   :load-path ("elisp/combobulate"))
 
 
