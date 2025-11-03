2e96f61500bfa7440f940761636ec5fdb5a31115
William Bert <william.bert@gmail.com>
2025-11-03 12:21:37 -0500

    Update .dir-locals.el.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,6 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((nil
-  ;; (eval . (progn
-  ;;           (treemacs-do-switch-workspace
-  ;;            (treemacs--select-workspace-by-name "Default"))))
-  ))
+((nil .
+      ((tags-table-list . ()))
+))
