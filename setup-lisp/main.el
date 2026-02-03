85e413da4e3d9170be0bb8939be3b75c727bba37
William Bert <william.bert@gmail.com>
2026-02-03 17:12:25 -0500

    Use marginalia.

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -4257,6 +4257,22 @@ If BUFFER-NAME doesn't exist, signal MISSING-MESSAGE."
   ;; :bind ("C-x C-d" . consult-dir)
   )
 
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
 (provide 'main)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
