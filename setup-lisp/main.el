c05fcc01910557906ab8b0c43dec8fa80e722a24
William Bert <william.bert@gmail.com>
2026-02-09 15:35:32 -0500

    Fix setup-jtsx-mode.

--- setup-lisp/main.el
+++ setup-lisp/main.el
@@ -2852,9 +2852,7 @@ If buffer is not visiting a file, do nothing."
 (require 'setup-js-mode)
 (require 'setup-ts-mode)
 ;; (require 'setup-rjsx-mode)
-(require 'setup-jtsx-mode)
 (require 'setup-eglot)
-(require 'setup-jtsx-mode)
 
 ;; TODO ensure these come after flycheck, otherwise checkers may not be set up correctly
 (eval-after-load 'js2-mode '(require 'setup-js2-mode))
@@ -4002,6 +4000,8 @@ questions.  Else use completion to select the tab to switch to."
   :config
   (treesit-auto-add-to-auto-mode-alist treesit-auto-langs)
   (global-treesit-auto-mode))
+
+(require 'setup-jtsx-mode)
 
 (use-package combobulate
   ;; :disabled
